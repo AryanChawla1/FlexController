b61d6e06f22fa73a3cfe1c5f7b6b25e271160702
Aryan Chawla <71091105+AryanChawla1@users.noreply.github.com>
2021-06-11 02:03:09 -0400

    LCD and Game Creation
    The LCD has been created with the game. As of now, the Transmitting HM-10 Bluetooth Module is sending a random 1 or 0 every second, 1 representing a flex. The spikes move from left to right and the character can move between the two lanes to dodge the spikes. Collision detection has not been added either.

--- FlexController-main/FlexControllerReceive/FlexControllerReceive.ino
+++ FlexController-main/FlexControllerReceive/FlexControllerReceive.ino
@@ -31,6 +31,34 @@ struct Packet {
 //Pins for LCD
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+//Custom Character that User Controls
+byte character[8] = {
+  0b01111,
+  0b01000,
+  0b01000,
+  0b11111,
+  0b11111,
+  0b11111,
+  0b01001
+};
+
+byte spike[8] = {
+  0b00000,
+  0b00000,
+  0b00000,
+  0b00000,
+  0b00100,
+  0b01110,
+  0b11111
+};
+
+int position_grid[2][16] = {
+ { 2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0 },
+ { 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 }
+};
+
+int character_position = 0;
  
 void setup() {
   // Start Serial Monitor for feedback
@@ -41,6 +69,12 @@ void setup() {
 
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
+
+  lcd.clear();
+  lcd.createChar(0, character);
+  lcd.createChar(1, spike);
+
+  create_game();
 }
  
 void loop() {
@@ -48,7 +82,7 @@ void loop() {
   bluetooth_receive();  
 
   // Necessary forced delay
-  delay(1);
+  delay(1000);
 }
 
 // Function responsible for receiving data over bluetooth
@@ -58,13 +92,76 @@ void bluetooth_receive() {
     // Read in the appropriate number of bytes to fit our Packet
     BTSerial.readBytes((byte *) & pkt,sizeof(Packet));
     
-    // Print packet (debug)
-    print_packet();
+   check_change();
   }   
 }
 
-// Function to print packet data (debug)
-void print_packet() {  
-  lcd.setCursor(0,1);
-  lcd.print(pkt.flex);
+
+
+void check_change() {
+  if (pkt.flex == 1) {
+    if (character_position == 0) {
+      lcd.setCursor(0,0);
+      lcd.print(" ");
+      lcd.setCursor(0,1);
+      lcd.write(byte(0));
+      character_position = 1;
+      position_grid[1][0] = 2;
+      position_grid[0][0] = 0;
+    }
+    else {
+      lcd.setCursor(0,1);
+      lcd.print(" ");
+      lcd.setCursor(0,0);
+      lcd.write(byte(0));
+      character_position = 0;
+      position_grid[1][0] = 0;
+      position_grid[0][0] = 2;
+    }
+  }
+
+  move_spikes();
+  create_game();
+}
+
+void create_game() {
+  lcd.clear();
+  
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 16; j++) {
+      if (position_grid[i][j] == 2) {
+        lcd.setCursor(j, i);
+        lcd.write(byte(0));
+      }
+      else if (position_grid[i][j] == 1) {
+        lcd.setCursor(j, i);
+        lcd.write(byte(1));
+      }
+    }
+  }
+}
+
+void move_spikes() {
+  for (int i = 0; i < 2; i++) {
+    for (int j = 2; j < 16; j++) {
+      position_grid[i][j-1] = position_grid[i][j];
+    }
+  }
+
+  if (position_grid[0][15] == 1 || position_grid[1][15] == 1) {
+    position_grid[0][15] = 0;
+    position_grid[1][15] = 0;
+  }
+  else {
+    int spike_placer = random(0,2);
+
+    if (spike_placer == 1) {
+      position_grid[1][15] = 1;
+      position_grid[0][15] = 0;
+    }
+    else {
+      position_grid[1][15] = 0;
+      position_grid[0][15] = 1;
+    }
+  }
 }
