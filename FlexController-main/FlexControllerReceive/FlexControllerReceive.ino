4c5cece2b139fad0250b1df5c3d8e66372d2e3ab
Aryan Chawla <71091105+AryanChawla1@users.noreply.github.com>
2021-06-10 23:47:56 -0400

    Add files via upload

--- FlexController-main/FlexControllerReceive/FlexControllerReceive.ino
+++ FlexController-main/FlexControllerReceive/FlexControllerReceive.ino
@@ -3,8 +3,8 @@
  * @Name: FlexControllerReceive
  * 
  * @Function: Receive data in form of boolean from other HM-10 Sensor
+ *            It also has the LCD for the game code
  * 
- * @Note: Sending it to Game code needed
  */
 
 // Receiving data in the form of a struct from transmitter
@@ -14,17 +14,23 @@
 // Library to make a Software UART
 #include <SoftwareSerial.h>
 
-#define RX 0
-#define TX 1
+//Library to work with the LCD
+#include <LiquidCrystal.h>
 
+#define RX 9
+#define TX 8
 #define BAUDRATE 9600
- 
+
 SoftwareSerial BTSerial(RX, TX); // (RX, TX)
 
 // Struct to hold the data we want to receive
 struct Packet {
   int flex;
 } pkt; // Instantiate a Packet struct
+
+//Pins for LCD
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
  
 void setup() {
   // Start Serial Monitor for feedback
@@ -32,6 +38,9 @@ void setup() {
 
   // HM-10 virtual UART
   BTSerial.begin(BAUDRATE);
+
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
 }
  
 void loop() {
@@ -56,7 +65,6 @@ void bluetooth_receive() {
 
 // Function to print packet data (debug)
 void print_packet() {  
-  Serial.print("RX: The Result is: ");
-  Serial.print(pkt.flex);
-  Serial.print("\n");
+  lcd.setCursor(0,1);
+  lcd.print(pkt.flex);
 }
